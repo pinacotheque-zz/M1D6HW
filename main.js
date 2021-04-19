4a80e4c0a2d60e67620fa551324058d25fa591fd
Gamze Güneri <62291935+pinacotheque@users.noreply.github.com>
2021-04-19 17:24:10 +0300

    js added  Please enter the commit message for your changes. Lines starting

---
+++ main.js
@@ -0,0 +1,7 @@
+let elementSelectedByCssQuery = document.querySelector(`h1`)
+const changeClass = function () {
+elementSelectedByCssQuery.innerText = `Gamze's BookStore.`
+}
+const changeBackground = function () {
+    document.body.style.backgroundColor = "rgb(214, 226, 189)"
+    }
